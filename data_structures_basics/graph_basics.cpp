ef568dd8a3f31e8657a18ce7cc37c039365afe4d
mangeshaher <mangeshaherngp@gmail.com>
2020-10-03 21:18:27 +0530

    Added DFS code in Graph

--- data_structures_basics/graph_basics.cpp
+++ data_structures_basics/graph_basics.cpp
@@ -14,15 +14,41 @@ void printAdjList(std::vector<int> v[],int vertices){
 	}
 }
 
+void BFS(std::vector<int> adj[],int vertices, int startIndex){
+	bool visited[vertices+1];
+	for(int i=0;i<vertices;i++){
+		visited[i] = false;
+	}
+	queue<int> que;
+	que.push(startIndex);
+	cout<<"BFS is :- ";
+	while(!que.empty()){
+		int front = que.front();
+		if(!visited[front]){
+			cout<<front<<" ";
+			for(int i=0;i<adj[front].size();i++){
+				if(!visited[adj[front].at(i)]){
+					que.push(adj[front].at(i));
+				}
+			}
+			visited[front]=true;
+		}
+		que.pop();
+	}
+	cout<<endl;
+}
+
 // Driver program 
 int main() 
 { 
 	int n,u,v;
 	cin>>n;
-	std::vector<int> adj[n];
+	std::vector<int> adj[n+1];
 	for(int i=0;i<n;i++){
 		cin>>u>>v;
 		adj[u].push_back(v);
+		adj[v].push_back(u);
 	}
 	printAdjList(adj,n);
+	BFS(adj,n,6);
 }
