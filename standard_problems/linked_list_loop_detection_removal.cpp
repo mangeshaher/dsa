e1e0d5e9ed36eba1359370c34ec5c8595c324ec1
mangeshaher <mangeshaherngp@gmail.com>
2020-10-07 23:00:50 +0530

    Added loop removal logic and theory

--- standard_problems/linked_list_loop_detection_removal.cpp
+++ standard_problems/linked_list_loop_detection_removal.cpp
@@ -52,8 +52,40 @@ bool detectLoop(Node* head){
 	return flag;
 }
 
+/*
+Let there be a nodes before loop start
+Let the two pointers meet after k steps after loop begins
+Let the cycle be of n nodes
+Distance travelled by slow be :- a + x*n + k
+Distance travelled by fast be :- a + y*n + k
+Distance travelled by fast is 2* dustance travelled by slow
+a + y*n + k = 2 * (a + x*n + k)
+(y-2*x)*n = a + k
+So the point to highlight is a+k is multiple of n
+If fast travels from meeting point and slow travels from head
+after a steps of slow fast would have completed k+a steps from beginning of cycle
+thus fast will complete cycle and slow will begin cycle 
+and hence they will always meet at starting point of cycle
+*/
 Node* removeLoop(Node* head){
-
+	Node* slow = head;
+	Node* fast = head;
+	Node* meetPoint;
+	while(slow&&fast&&fast->next){
+		slow = slow->next;
+		fast = fast->next->next;
+		if(slow==fast){
+			meetPoint = slow;
+			break;
+		}
+	}
+	slow = head;
+	while(slow->next!=meetPoint->next){
+		slow=slow->next;
+		meetPoint=meetPoint->next;
+	}
+	meetPoint->next=NULL;
+	return head;
 }
 
 // Driver program 
@@ -73,4 +105,5 @@ int main()
 	last->next = head->next->next;
 	cout<<detectLoop(head)<<endl;
 	head = removeLoop(head);
+	printList(head);
 } 
