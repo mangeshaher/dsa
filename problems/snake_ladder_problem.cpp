df7c5bbfb34d942152ab73d249d67c59d9b6501d
mangeshaher <mangeshaherngp@gmail.com>
2020-10-09 00:51:24 +0530

    Snake ladder solution using BFS O(n)

--- problems/snake_ladder_problem.cpp
+++ problems/snake_ladder_problem.cpp
@@ -2,24 +2,54 @@
 #include <bits/stdc++.h> 
 using namespace std; 
 
+class Node{
+public:
+	int i;
+	int length;
+	Node(int a,int b){
+		i = a;
+		length = b;
+	}
+};
+
 // Driver program 
 int main() 
 { 
 	int n,a,b;
+	bool visited[31];
+	queue<Node*> q;
 	unordered_map<int,int> map;
 	cin>>n;  
 	for(int i=0;i<n;i++){
 		cin>>a>>b;
 		map[a]=b;
 	}
-	int dp[31];
 	for(int i=1;i<31;i++){
-		dp[i] = min(dp[i],i/6 +1);
-		if(map.find(i)!=map.end()){
-			dp[map[i]] = min(dp[map[i]],dp[i]);
-		}
+		visited[i] = false;
 	}
-	for(int i=1;i<31;i++){
-		cout<<dp[i]<<" ";
+	q.push(new Node(1,0));
+	int count = 0;
+	int nodesInserted = 1;
+	while(!q.empty()){
+		Node* front = q.front();
+		if(front->i==30){
+			cout<<"Minimum Length is :- "<<front->length<<endl;
+			break;
+		}
+		for(int i =front->i+1;i<=front->i+6;i++){
+			if(!visited[i]){
+				visited[i]=true;
+				if(map.find(i)!=map.end()&&!visited[map[i]]){
+					q.push(new Node(map[i],front->length + 1));
+					visited[map[i]]=true;
+				}
+				else{
+					if(map.find(i)==map.end()){
+						q.push(new Node(i,front->length + 1));
+					}
+				}
+			}
+		}
+		q.pop();
 	}
 } 
